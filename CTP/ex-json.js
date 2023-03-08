3534d13208fa40ff66c91749b8413505cb1fb6a0
TimCode78 <125401262+TimCode78@users.noreply.github.com>
2023-03-08 17:42:34 +0100

    Add files via upload

---
+++ CTP/ex-json.js
@@ -0,0 +1,20 @@
+// Exemple d'objet JSON identique à l'énoncé
+var objetListeLecture = {
+  "createur": "Maxime Folschette",
+  "nom": "Mon best-of perso",
+  "date": "17/03/2022",
+  "liste_lecture": [
+    { "titre": "Trains",
+      "artiste": "Porcupine Tree",
+      "album": "In Absentia",
+      "image": "ressources/porcupine-tree_in-absentia.jpg",
+      "lien": "https://www.youtube.com/watch?v=ut3OQzfaeSw"
+    },
+    { "titre": "Black Dog",
+      "artiste": "Led Zeppelin",
+      "album": "Led Zeppelin IV",
+      "lien": "https://www.youtube.com/watch?v=yBuub4Xe1mw"
+    }
+  ]
+};
+
---
+++ CTP/script.js
@@ -0,0 +1,26 @@
+var objetListeLecture = {
+    "createur": "Maxime Folschette",
+    "nom": "Mon best-of perso",
+    "date": "17/03/2022",
+    "liste_lecture": [
+      { "titre": "Trains",
+        "artiste": "Porcupine Tree",
+        "album": "In Absentia",
+        "image": "ressources/porcupine-tree_in-absentia.jpg",
+        "lien": "https://www.youtube.com/watch?v=ut3OQzfaeSw"
+      },
+      { "titre": "Black Dog",
+        "artiste": "Led Zeppelin",
+        "album": "Led Zeppelin IV",
+        "lien": "https://www.youtube.com/watch?v=yBuub4Xe1mw"
+      },
+      { "titre": "kk Boudin",
+        "artiste": "mamma ",
+        "album": "new V",
+        "lien": "https://www.youtube.com/watch?v=yBuub4Xe1mw"
+      }
+    ]
+  };
+  
+  var refInport=document.getElementById("import") ;
+  
